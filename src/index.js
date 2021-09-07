1c65d78563ed59efb4f41ddc90f60b7fc3f146f9
Chris <ChrisGavlon@gmail.com>
2021-09-07 11:40:18 -0500

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// your code here
+let form = document.querySelector('form')
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buildToDoList(event.target.new_task_description.value)
+    form.reset()
+})
+
+function buildToDoList(newtask){
+let p = document.createElement('p')
+let delbttn = document.createElement('button')
+delbttn.addEventListener('click', deletetask)
+delbttn.textContent = 'x'
+p.textContent = `${newtask} `
+p.appendChild(delbttn)
+console.log(p)
+document.querySelector('#tasks').appendChild(p)
+}
+console.log('DOM has loaded')
 });
+console.log('DOM loads')
+
+function deletetask(event){
+  event.target.parentNode.remove()
+}
